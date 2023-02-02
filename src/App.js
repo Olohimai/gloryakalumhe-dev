4d6c83014110be48829fc0eb57dc60ded88018e9
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-02-02 12:50:55 +0000

    installed-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-
+import React from "react";  
 function App() {
   return (
     <div className="App">
-    
+    <button type="button" className="btn btn-primary">Primary</button>
     </div>
   );
 }
-
+  
 export default App;
