b24fe959e4c29c7cb7ae294779c9e5a72b67e144
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-05-14 02:06:18 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="">
-      <Header id="home" />
+      <Header />
       <Skills />
       <Project />
       <About />
