fe3c376dcdff67eb284f90e3074d0ab221af3417
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-05-14 03:01:25 +0100

    wip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
             <Navbar />
             <div id='home' className='bg-yellow-100'>
                 <div className='px-6 lg:px-8 py-10'>
-                    <div className='mx-auto max-w-4xl py-32 sm:py-48 lg:py-20 text-left'>
+                    <div className='max-w-4xl py-32 sm:py-48 lg:py-20 text-left'>
                         <h1 className='text-4xl font-bold tracking-tight text-black sm:text-6xl'> Hi, I am <br></br><span className='text-yellow-200'>Glory Akalumhe</span>  </h1>
                         <span className='mt-6 text-lg leading-8 text-gray-600'>A Front-End Developer based in the Uk.</span>
                         <p className='mt-6 text-base leading-8 text-gray-600'>I describe Myself as a Passionate Developer who loves coding, open source, and the web platform . Aside from my job, I like to create and contribute to open source projects. That helps me to learn a ton of new stuff, grow as a developer and support other open source projects</p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,11 +4,11 @@ import SkillList from './SkillList'
 function Skills() {
     return (
         <div id='skills' className='px-6 lg:px-8 py-10'>
-            <div className='mx-auto max-w-2xl text-center'>
+            <div className='max-w-2xl text-center lg:text-left'>
                 <h2 className='text-4xl font-bold tracking-tight text-black'>
                     Take a look at my Skills.
                 </h2>
-                <p className='mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600 font-medium'>This lists all the technologies and programming languages I have acquired knowledge in until present and I am continuously expanding my knowledge and thus, this section may be updated frequently.</p>
+                <p className='mt-6 max-w-xl text-lg leading-8 text-gray-600 font-medium'>This lists all the technologies and programming languages I have acquired knowledge in until present and I am continuously expanding my knowledge and thus, this section may be updated frequently.</p>
             </div>
             <SkillList />
         </div>
