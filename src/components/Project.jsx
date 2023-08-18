8e473f48302232080396d7297df3b12831b1b5f4
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-08-18 12:30:03 +0100

    wip

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function Project() {
     return (
         <div id='project' className='bg-yellow-100'>
-            <div className='px-6 lg:px-8 py-10 mx-auto max-w-2xl md:max-w-7xl md:text-center'>
+            <div className='px-6 lg:px-8 py-10 mx-auto max-w-6xl md:max-w-7xl md:text-center'>
                 <h2 className='text-4xl font-bold tracking-tight text-black'>Recent Works  </h2>
                 <p className='mx-auto my-6 max-w-xl text-lg leading-8 text-gray-600 font-medium'>These are some projects I have worked on</p>
-                <ul className="py-4 grid grid-cols-2 lg:grid-cols-4 lg:gap-8 gap-8 text-xs lg:text-base ">
+                <ul className="py-4 grid grid-cols-2 lg:grid-cols-4 lg:gap-8 gap-4 text-xs lg:text-base ">
                     <li className='font-semibold'>digikraaft.ng: <span className='font-medium text-yellow-200'><a href='https://www.digikraaft.ng' className='cursor-pointer'>https://www.digikraaft.ng</a></span>
                     </li>
                     <li className='font-semibold'>onedata.ng: <span className='font-medium text-yellow-200'><a href='https://www.onedata.ng' className='cursor-pointer'>https://www.onedata.ng</a></span>
                     </li>
-                    <li className='font-semibold'>ips-ecd.uk.net: <br /><span className='font-medium text-yellow-200'><a href='https://www.ips-ecd.uk.net' className='cursor-pointer'>https://www.ips-ecd.uk.net.ng</a></span>
-                    </li>
+                    {/* <li className='font-semibold'>ips-ecd.uk.net: <br /><span className='font-medium text-yellow-200'><a href='https://www.ips-ecd.uk.net' className='cursor-pointer'>https://www.ips-ecd.uk.net.ng</a></span>
+                    </li> */}
                     <li className='font-semibold'>ips-ecd.uk.net: <br /> <span className='font-medium text-yellow-200'>
                         <a href='https://www.ips-ecd.uk.net' className='cursor-pointer'>https://www.ips-ecd.uk.net</a></span>
                     </li>
@@ -26,7 +26,8 @@ function Project() {
                     </li>
                     <li className='font-semibold'>Igoal.finance: <span className='font-medium text-yellow-200'><a href='https://www.igoal.finance/' className='cursor-pointer'>https://www.igoal.finance/</a></span>
                     </li>
-                    <li className='font-semibold'>prividaenergy.com: <span className='font-medium text-yellow-200'><a href='https://www.prividaenergy.com/' className='cursor-pointer'>https://www.prividaenergy.com/</a></span>
+                    <li className='font-semibold'>prividaenergy.com: <span className='font-medium text-yellow-200'><a href='https://www.prividaenergy.com/' className='cursor-pointer'>https://www.prividaenergy.com
+                    </a></span>
                     </li>
                 </ul>
             </div>
