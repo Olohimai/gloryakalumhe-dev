868a12f70291cf3ba2d78ec785f9831a29bc9cbb
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-08-18 13:08:43 +0100

    update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,7 @@ function Project() {
             <div className='px-6 lg:px-8 py-10 mx-auto max-w-6xl md:max-w-7xl md:text-center'>
                 <h2 className='text-4xl font-bold tracking-tight text-black'>Recent Works  </h2>
                 <p className='mx-auto my-6 max-w-xl text-lg leading-8 text-gray-600 font-medium'>These are some projects I have worked on</p>
-                <ul className="py-4 grid grid-cols-1 lg:grid-cols-4 lg:gap-8 gap-4 text-xs lg:text-base ">
+                <ul className="py-4 grid grid-cols-2 lg:grid-cols-4 lg:gap-8 gap-4 text-xs lg:text-base ">
                     <li className='font-semibold'>digikraaft.ng: <span className='font-medium text-yellow-200'><a href='https://www.digikraaft.ng' className='cursor-pointer'>https://www.digikraaft.ng</a></span>
                     </li>
                     <li className='font-semibold'>onedata.ng: <span className='font-medium text-yellow-200'><a href='https://www.onedata.ng' className='cursor-pointer'>https://www.onedata.ng</a></span>
